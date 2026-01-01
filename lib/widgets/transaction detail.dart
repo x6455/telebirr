e299908d9544216db536686b67505b35fee11dd5
X <129300554+x6455@users.noreply.github.com>
2026-01-01 11:08:11 -0800

    Update transaction detail.dart

--- lib/widgets/transaction detail.dart
+++ lib/widgets/transaction detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transaction_history_page.dart'; // Ensure this matches your filename
 
 class TransactionDetails extends StatelessWidget {
   const TransactionDetails({
@@ -7,22 +8,34 @@ class TransactionDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Text(
-          'Transaction Details',
-          textAlign: TextAlign.end,
-          style: TextStyle(
+    return GestureDetector(
+      // This makes the entire row clickable
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const TransactionHistoryPage(),
+          ),
+        );
+      },
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'Transaction Details',
+            textAlign: TextAlign.end,
+            style: TextStyle(
               color: Color.fromRGBO(19, 132, 185, 1),
-              fontWeight: FontWeight.bold),
-        ),
-        Icon(
-          Icons.keyboard_arrow_right_rounded,
-          color: Color.fromRGBO(19, 132, 185, 1),
-          weight: 12,
-        )
-      ],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Icon(
+            Icons.keyboard_arrow_right_rounded,
+            color: Color.fromRGBO(19, 132, 185, 1),
+            size: 24, // Note: Use 'size' for visual scale; 'weight' is for variable fonts
+          )
+        ],
+      ),
     );
   }
 }
