c3ae4d03133bc07621c9af568280192b4b6edc7b
X <129300554+x6455@users.noreply.github.com>
2026-02-19 11:47:53 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:telebirrbybr7/colors.dart';
 import 'package:telebirrbybr7/screens/main_screen.dart';
+import 'package:telebirrbybr7/screens/login_page.dart'; // 1. Import your new login file
 import 'package:telebirrbybr7/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // 🔔 Initialize local notifications
   await NotificationService.init();
-
   runApp(const MyApp());
 }
 
@@ -27,7 +25,8 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const MainScreen(),
+      // 2. Set LoginPage as the initial screen
+      home: const LoginPage(), 
     );
   }
 }
