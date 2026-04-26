5e73d60a74ea57d8cc7d6a7ae28b371d9c9cb9d2
X <129300554+x6455@users.noreply.github.com>
2026-04-26 17:29:16 +0300

    Change greeting from 'HELEN' to 'DANIEL'

--- lib/widgets/user_introduction.dart
+++ lib/widgets/user_introduction.dart
@@ -16,7 +16,7 @@ class UserIntroduction extends StatelessWidget {
            
         ),
         Text(
-          "Selam, HELEN",
+          "Selam, DANIEL",
           style: TextStyle(
             color: Colors.white70,
             fontSize: 18,
