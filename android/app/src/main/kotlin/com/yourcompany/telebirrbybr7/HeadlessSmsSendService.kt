5eb1c30745ffb1687e38b5b2efc0e2de4ef75bbf
X <129300554+x6455@users.noreply.github.com>
2026-04-27 21:07:48 +0300

    Update HeadlessSmsSendService.kt

--- android/app/src/main/kotlin/com/yourcompany/telebirrbybr7/HeadlessSmsSendService.kt
+++ android/app/src/main/kotlin/com/yourcompany/telebirrbybr7/HeadlessSmsSendService.kt
@@ -1,11 +1,15 @@
 package com.yourcompany.telebirrbybr7
 
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
+import android.service.quicksettings.TileService
 
-class HeadlessSmsSendService : Service() {
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-    }
+/**
+ * This service is required for your app to be eligible as the default SMS app.
+ * Without this, Android will NOT allow your app to become the default SMS app.
+ * 
+ * This service handles the "quick reply" feature from notifications.
+ * Even if you don't use quick reply, this class MUST exist.
+ */
+class HeadlessSmsSendService : TileService() {
+    // This class can remain empty.
+    // Its existence alone satisfies Android's requirement for default SMS apps.
 }
